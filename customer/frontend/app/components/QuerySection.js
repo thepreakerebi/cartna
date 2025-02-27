65d50de4ce7d1625d377df050fd7d88bf2e086a2
Elvis Kerebi <kerebipreye@gmail.com>
2025-02-27 12:06:21 +0200

    edited the examples to try out on querySelection

--- customer/frontend/app/components/QuerySection.js
+++ customer/frontend/app/components/QuerySection.js
@@ -64,10 +64,10 @@ export default function QuerySection() {
             <h3>Try these examples:</h3>
             <ul>
               <li>{"I need rice, cooking oil, and sugar"}</li>
-              <li>{"Looking for fresh vegetables for soup"}</li>
-              <li>{"Baby food and diapers"}</li>
-              <li>{"Breakfast cereals and milk"}</li>
-              <li>{"Snacks and soft drinks for a party"}</li>
+              <li>{"Looking for coca-cola"}</li>
+              <li>{"I want some beef"}</li>
+              <li>{"Iyange milk"}</li>
+              <li>{"I need colgate toothpaste"}</li>
             </ul>
           </div>
         </>
